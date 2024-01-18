077ee8195543172a4056f8a1fbd7603b6e5c0cf3
Andreas Birch <andreasbirch1998@hotmail.com>
2024-01-18 22:42:18 +0100

    Update MerchantService.java

--- src/test/java/behaviourtests/merchant/MerchantService.java
+++ src/test/java/behaviourtests/merchant/MerchantService.java
@@ -1,16 +1,21 @@
 package behaviourtests.merchant;
 
+
 import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
 import javax.ws.rs.client.Entity;
 import javax.ws.rs.client.WebTarget;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
 public class MerchantService {
 
 	public DTUPayAccount register(DTUPayAccount acc) {
 		Client client = ClientBuilder.newClient();
 		WebTarget r = client.target("http://localhost:8080/merchant");
-		var response = r.path("/register").request().post(Entity.json(acc), DTUPayAccount.class);
-		return response;
+		Response response = r.path("/register")
+				.request()
+				.post(Entity.entity(acc, MediaType.APPLICATION_JSON));
+		return response.readEntity(DTUPayAccount.class);
 	}
 }
