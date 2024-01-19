df67a83505d895da72276e06c9515fc52c07f6cc
MarianCCulea <marianclaudiu.culea@gmail.com>
2024-01-19 16:52:55 +0100

    Fix7

--- src/test/java/behaviourtests/customer/CustomerService.java
+++ src/test/java/behaviourtests/customer/CustomerService.java
@@ -5,6 +5,7 @@ import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
 import javax.ws.rs.client.Entity;
 import javax.ws.rs.client.WebTarget;
+import javax.ws.rs.core.MediaType;
 
 
 public class CustomerService {
@@ -12,14 +13,14 @@ public class CustomerService {
 	public DTUPayAccount register(DTUPayAccount c) {
 		Client client = ClientBuilder.newClient();
 		WebTarget r = client.target("http://localhost:8080/");
-		var response = r.path("customers").request().post(Entity.json(c), DTUPayAccount.class);
+		var response = r.path("customers").request().post(Entity.entity(c, MediaType.APPLICATION_JSON), DTUPayAccount.class);
 		return response;
 	}
 
 	public String[] generateTokens(DTUPayAccount c) {
 		Client client = ClientBuilder.newClient();
 		WebTarget r = client.target("http://localhost:8080/customer/");
-		var response = r.path("tokens").request().post(Entity.json(c), String[].class);
+		var response = r.path("tokens").request().post(Entity.entity(c, MediaType.APPLICATION_JSON), String[].class);
 		return response;
 	}
 }
--- src/test/java/behaviourtests/merchant/MerchantService.java
+++ src/test/java/behaviourtests/merchant/MerchantService.java
@@ -5,6 +5,7 @@ import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
 import javax.ws.rs.client.Entity;
 import javax.ws.rs.client.WebTarget;
+import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
 public class MerchantService {
@@ -12,7 +13,7 @@ public class MerchantService {
 	public DTUPayAccount register(DTUPayAccount acc) {
 		Client client = ClientBuilder.newClient();
 		WebTarget r = client.target("http://localhost:8080/merchant");
-		var response = r.path("/register").request().post(Entity.json(acc), DTUPayAccount.class);
+		var response = r.path("/register").request().post(Entity.entity(acc, MediaType.APPLICATION_JSON), DTUPayAccount.class);
 		return response;
 	}
 }
