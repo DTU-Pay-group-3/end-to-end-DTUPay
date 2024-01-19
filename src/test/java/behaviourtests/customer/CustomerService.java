3e2f17d4deb8a94ebd0491cf31974987bed0505c
Andreas Birch <andreasbirch1998@hotmail.com>
2024-01-19 11:56:42 +0100

    Revert "Changed rest service from javax to jakarta"
    This reverts commit 52cb792e7d70d28bf5858978180edb13f8b1aabf.

--- src/test/java/behaviourtests/customer/CustomerService.java
+++ src/test/java/behaviourtests/customer/CustomerService.java
@@ -1,28 +1,26 @@
 package behaviourtests.customer;
 
 import behaviourtests.model.DTUPayAccount;
-import jakarta.ws.rs.client.Client;
-import jakarta.ws.rs.client.ClientBuilder;
-import jakarta.ws.rs.client.Entity;
-import jakarta.ws.rs.client.WebTarget;
 
-import static jakarta.ws.rs.core.MediaType.APPLICATION_JSON;
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.Entity;
+import javax.ws.rs.client.WebTarget;
 
 public class CustomerService {
 
 	public DTUPayAccount register(DTUPayAccount c) {
 		Client client = ClientBuilder.newClient();
-		WebTarget r = client.target("http://fm-03.compute.dtu.dk:8080/");
-		var response = r.path("customers")
-				.request()
-				.post(Entity.entity(c, APPLICATION_JSON));
-		return response.readEntity(DTUPayAccount.class);
+		WebTarget r = client.target("http://localhost:8080/");
+		var response = r.path("customers").request().post(Entity.json(c), DTUPayAccount.class);
+		return response;
 	}
 
 	public String[] generateTokens(DTUPayAccount c) {
-		Client client = ClientBuilder.newClient();
-		WebTarget r = client.target("http://fm-03.compute.dtu.dk:8080/customers/");
-		var response = r.path("tokens").request().post(Entity.json(c), String[].class);
-		return response;
+//		Client client = ClientBuilder.newClient();
+//		WebTarget r = client.target("http://localhost:8080/customer/");
+//		var response = r.path("tokens").request().post(Entity.json(c), String[].class);
+//		return response;
+		return new String[]{"asd","dsadas"};
 	}
 }
