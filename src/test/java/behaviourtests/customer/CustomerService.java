f97a4df8a0a80c4080f13a6f49ad047998f7ca02
Andreas Birch <andreasbirch1998@hotmail.com>
2024-01-19 14:54:42 +0100

    Change from javax to jakarta

--- src/test/java/behaviourtests/customer/CustomerService.java
+++ src/test/java/behaviourtests/customer/CustomerService.java
@@ -1,12 +1,11 @@
 package behaviourtests.customer;
 
 import behaviourtests.model.DTUPayAccount;
-
-import javax.ws.rs.client.Client;
-import javax.ws.rs.client.ClientBuilder;
-import javax.ws.rs.client.Entity;
-import javax.ws.rs.client.WebTarget;
-import javax.ws.rs.core.MediaType;
+import jakarta.ws.rs.client.Client;
+import jakarta.ws.rs.client.ClientBuilder;
+import jakarta.ws.rs.client.Entity;
+import jakarta.ws.rs.client.WebTarget;
+import jakarta.ws.rs.core.MediaType;
 
 public class CustomerService {
 
