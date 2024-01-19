77550f2f6f1c9f221af2c4c9f99208cca0cf2aba
MarianCCulea <marianclaudiu.culea@gmail.com>
2024-01-19 16:10:26 +0100

    Fix4

--- src/test/java/behaviourtests/model/DTUPayAccount.java
+++ src/test/java/behaviourtests/model/DTUPayAccount.java
@@ -3,12 +3,14 @@ package behaviourtests.model;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import java.util.Objects;
 import java.util.UUID;
 
 @Data
 @Getter
+@NoArgsConstructor
 public class DTUPayAccount {
 
     private String id;
@@ -16,67 +18,12 @@ public class DTUPayAccount {
     private String cprNumber;
     private String accountNumber;
 
-    public DTUPayAccount() {
-    }
-
     public DTUPayAccount(String firstName, String lastName, String cprNumber, String accountNumber) {
-        this.id = UUID.randomUUID().toString();
+        this.id = "";
         this.firstName = firstName;
         this.lastName = lastName;
         this.cprNumber = cprNumber;
         this.accountNumber = accountNumber;
     }
 
-    public String getId() {
-        return id;
-    }
-
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    public String getCprNumber() {
-        return cprNumber;
-    }
-
-    public void setCprNumber(String cprNumber) {
-        this.cprNumber = cprNumber;
-    }
-
-    public String getAccountNumber() {
-        return accountNumber;
-    }
-
-    public void setAccountNumber(String accountNumber) {
-        this.accountNumber = accountNumber;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        DTUPayAccount that = (DTUPayAccount) o;
-        return Objects.equals(id, that.id);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(id);
-    }
 }
