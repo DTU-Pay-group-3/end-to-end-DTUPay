b108c73db0814943e7662eeb98431fe1edbb0915
MarianCCulea <marianclaudiu.culea@gmail.com>
2024-01-19 15:49:50 +0100

    Fix3

--- src/test/java/behaviourtests/customer/CustomerService.java
+++ src/test/java/behaviourtests/customer/CustomerService.java
@@ -1,28 +1,25 @@
 package behaviourtests.customer;
 
 import behaviourtests.model.DTUPayAccount;
-import jakarta.ws.rs.client.Client;
-import jakarta.ws.rs.client.ClientBuilder;
-import jakarta.ws.rs.client.Entity;
-import jakarta.ws.rs.client.WebTarget;
-import jakarta.ws.rs.core.MediaType;
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.Entity;
+import javax.ws.rs.client.WebTarget;
+
 
 public class CustomerService {
 
 	public DTUPayAccount register(DTUPayAccount c) {
 		Client client = ClientBuilder.newClient();
 		WebTarget r = client.target("http://localhost:8080/");
-		var response = r.path("customers")
-				.request()
-				.post(Entity.entity(c, MediaType.APPLICATION_JSON));
-		return response.readEntity(DTUPayAccount.class);
+		var response = r.path("customers").request().post(Entity.json(c), DTUPayAccount.class);
+		return response;
 	}
 
 	public String[] generateTokens(DTUPayAccount c) {
-//		Client client = ClientBuilder.newClient();
-//		WebTarget r = client.target("http://localhost:8080/customer/");
-//		var response = r.path("tokens").request().post(Entity.json(c), String[].class);
-//		return response;
-		return new String[]{"asd","dsadas"};
+		Client client = ClientBuilder.newClient();
+		WebTarget r = client.target("http://localhost:8080/customer/");
+		var response = r.path("tokens").request().post(Entity.json(c), String[].class);
+		return response;
 	}
 }
--- src/test/java/behaviourtests/merchant/MerchantService.java
+++ src/test/java/behaviourtests/merchant/MerchantService.java
@@ -1,21 +1,18 @@
 package behaviourtests.merchant;
 
 import behaviourtests.model.DTUPayAccount;
-import jakarta.ws.rs.client.Client;
-import jakarta.ws.rs.client.ClientBuilder;
-import jakarta.ws.rs.client.Entity;
-import jakarta.ws.rs.client.WebTarget;
-import jakarta.ws.rs.core.MediaType;
-import jakarta.ws.rs.core.Response;
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.Entity;
+import javax.ws.rs.client.WebTarget;
+import javax.ws.rs.core.Response;
 
 public class MerchantService {
 
 	public DTUPayAccount register(DTUPayAccount acc) {
 		Client client = ClientBuilder.newClient();
 		WebTarget r = client.target("http://localhost:8080/merchant");
-		Response response = r.path("/register")
-				.request()
-				.post(Entity.entity(acc, MediaType.APPLICATION_JSON));
-		return response.readEntity(DTUPayAccount.class);
+		var response = r.path("/register").request().post(Entity.json(acc), DTUPayAccount.class);
+		return response;
 	}
 }
